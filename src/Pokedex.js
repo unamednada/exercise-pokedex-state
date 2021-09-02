20677bcaa122452eba21368de3ed0e996f9a72cf
unamednada <dias.gustavo.dias@gmail.com>
2021-09-02 18:28:12 -0300

    Add button and change pokedex to display pokemon according to state

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,23 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      pokemon: 0,
     }
+  }
+
+  handleClick
+  render() {
+      return (
+          <div className="pokedex">
+            <Pokemon key={this.props.pokemons[this.state.pokemon].id} pokemon={this.props.pokemons[this.state.pokemon]} />
+            <button>Next</button>
+          </div>
+      );
+  }
 }
 
 export default Pokedex;
