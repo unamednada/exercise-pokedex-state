f9779def8330e6849ecbfbd477f6523e6a7dd541
unamednada <dias.gustavo.dias@gmail.com>
2021-09-03 15:16:51 -0300

    Create handlefilter for psychic pokemon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import Pokemon from './Pokemon';
-
+import React from "react";
+import Pokemon from "./Pokemon";
 
 // Inicializar estado chave filterArray como o this.props.pokemons e modificar o estado com handleFilter
 // Passar o estado como valor do prop pokemon no componente Pokemon
@@ -11,42 +10,50 @@ import Pokemon from './Pokemon';
 class Pokedex extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       pokemon: 0,
-      filterArray: this.props.pokemons,
-    }
+      filter: "Psychic",
+      pokemons: this.props.pokemons,
+    };
+
     this.handleClick = this.handleClick.bind(this);
-    this.handleFilter = this.handleFilter(this);
+    this.handleFilter = this.handleFilter.bind(this);
   }
 
   handleClick() {
-    if (this.state.pokemon < this.props.pokemons.length - 1) {
-      this.setState((state) => ({
-        pokemon: state.pokemon + 1,
-      }));
-    } else {
-      this.setState({
+    this.setState((state) => {
+      if (this.state.pokemon < this.state.pokemons.length - 1) {
+        return {
+          pokemon: state.pokemon + 1,
+        }
+      }
+      return {
         pokemon: 0,
-      })
-    } 
+      }  
+    })
   }
 
   handleFilter() {
-    this.setState((state) => ({
-      pokemon: 0,
-      filterArray: this.state.filterArray.filter(({ type }) => type === 'Fire'),
-    }))
-  } 
+    this.setState((state) => {
+      return {
+        pokemon: 0,
+        pokemons: this.props.pokemons.filter((pokemon) => pokemon.type === this.state.filter),
+      }
+    })
+  }
 
   render() {
-      return (
-          <div className="pokedex">
-            <Pokemon pokemon={this.state.filterArray[this.state.pokemon]} />
-            <button onClick={this.handleFilter}>Fire</button>
-            <button onClick={this.handleClick}>Next</button>
-          </div>
-      );
+
+    return (
+      <div className="pokedex">
+        <Pokemon pokemon={this.state.pokemons[this.state.pokemon]} />
+        <button onClick={this.handleFilter}>Psychic</button>
+        <button onClick={this.handleClick}>
+          Next
+        </button>
+      </div>
+    );
   }
 }
 
